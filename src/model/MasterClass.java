03f17cf6b210d68b48761f65a192c40709571fbf
ander2812 <ander2812@gmail.com>
2021-04-23 17:36:35 -0500

    method readFiles added

--- src/model/MasterClass.java
+++ src/model/MasterClass.java
@@ -1,14 +1,52 @@
 package model;
+import exceptions.RepeatedElementException;
 import structs.NodeTree;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class MasterClass {
-    private NodeTree<Integer,Integer> points;
+    private NodeTree<Double,ArrayList<Long>> points;
+    private NodeTree<Integer,ArrayList<Long>> f;
+    private NodeTree<Integer,ArrayList<Long>> a;
+    private NodeTree<Integer,ArrayList<Long>> l;
+    private NodeTree<Integer,ArrayList<Long>> z;
+    private File file;
 
     public MasterClass() {
         points = new NodeTree<>();
+        f = new NodeTree<>();
+        a = new NodeTree<>();
+        l = new NodeTree<>();
+        z = new NodeTree<>();
     }
 
-    public void search(){
+    public void readFiles(File file) throws IOException, RepeatedElementException {
+        this.file = file;
+        RandomAccessFile raf = new RandomAccessFile(file, "r");
+
+        raf.readLine();
+        String temp;
+        String[] cell;
+        long pos;
+
+        do {
+            pos = raf.getFilePointer();
+            temp = raf.readLine();
+            cell = temp.split(",");
+
+            if(points.search(Double.parseDouble(cell[7])) == null){
+                points.add(Double.parseDouble(cell[7]), new ArrayList<Long>());
+            }
+            points.search(Double.parseDouble(cell[7])).add(pos);
+
+        }while(temp != null);
+
+
+
+
+        raf.close();
+
 
     }
 }
