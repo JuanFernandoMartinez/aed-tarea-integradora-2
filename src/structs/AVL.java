d49845e981540ba40790781a6fdcc0478a4f8d56
ander2812 <ander2812@gmail.com>
2021-04-24 17:35:03 -0500

    MasterClass and Structs modified

--- src/structs/AVL.java
+++ src/structs/AVL.java
@@ -3,50 +3,30 @@ package structs;
 import java.util.ArrayList;
 
 public class AVL<K extends Comparable<K>,V> implements BSTInterface<K, V> {
-	
+
 	private Node<K,V> root;
 	private int size;
-	
+
 	public AVL() {
 		size = 0;
-		
+
 	}
-	
 
 	@Override
-	public boolean add(K k, V v) {
-		if (root == null)
-		{
-			root = new Node<>(k,v,true);
-			size++;
-			return true;
-		}else
-		{
-			boolean status = root.add(k, v);
-			if (status)
-			{
-				size++;
-				return true;
-			}else
-			{
-				return false;
-			}
-		 
-		}
+	public boolean add(K key, V value) {
+		return false;
 	}
 
-	
+	@Override
+	public boolean update(K key, V value) {
+		return false;
+	}
+
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
@@ -61,18 +41,18 @@ public class AVL<K extends Comparable<K>,V> implements BSTInterface<K, V> {
 		return false;
 	}
 
-	@Override
+
 	public Node<K, V> searchNode(K k) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ArrayList<V> search(K k) {
+	public V search(K k) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-	
+
+
 }
--- src/structs/BST.java
+++ src/structs/BST.java
@@ -1,95 +1,289 @@
 package structs;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
-public class BST<K extends Comparable<K>,V> implements BSTInterface<K, V> {
-	
-	private Node<K,V> root;
-	private int size;
-	
+public class BST<K extends Comparable<K>, V> implements BSTInterface<K, V> {
+
+	protected Node<K, V> root;
+
+	private HashSet<K> setKeys;
+
+	protected int weight;
+
 	public BST() {
-		size = 0;
-		
+
+		weight = 0;
+		setKeys = new HashSet<K>();
+
 	}
 
-	@Override
-	public boolean add(K k, V v) {
-		if (root == null)
-		{
-			root = new Node<>(k,v);
-			size++;
-			return true;
-		}else
-		{
-			boolean status = root.add(k, v);
-			if (status)
-			{
-				size++;
-				return true;
-			}else
-			{
-				return false;
+	public boolean add(K key, V value) {
+
+		boolean r = addBase(key, value) != null;
+
+		if (r) {
+			setKeys.add(key);
+		}
+
+		return r;
+	}
+
+	protected Node<K, V> addBase(K key, V value) {
+
+		Node<K, V> newNode = new Node<K, V>(key, value);
+
+		if (root != null) {
+			return addRecursive(key, value, root, newNode);
+		} else {
+			root = newNode;
+			weight++;
+			return root;
+		}
+	}
+
+	private Node<K, V> addRecursive(K key, V value, Node<K, V> currentNode, Node<K, V> newNode) {
+
+		if (key.compareTo(currentNode.getKey()) > 0) {
+
+			Node<K, V> right = currentNode.getRight();
+
+			if (right != null) {
+				Node<K, V> addedNode = addRecursive(key, value, right, newNode);
+
+				if (addedNode != null) {
+					right.update();
+				}
+
+				return addedNode;
 			}
-		 
+
+			else {
+				currentNode.setRight(newNode);
+				newNode.setParent(currentNode);
+				weight++;
+				return newNode;
+			}
+		} else if (key.compareTo(currentNode.getKey()) < 0) {
+			Node<K, V> left = currentNode.getLeft();
+
+			if (left != null) {
+				Node<K, V> addedNode = addRecursive(key, value, left, newNode);
+
+				if (addedNode != null) {
+					left.update();
+				}
+
+				return addedNode;
+			}
+
+			else {
+				currentNode.setLeft(newNode);
+				newNode.setParent(currentNode);
+				weight++;
+				return newNode;
+			}
+		} else {
+			return null;
 		}
 	}
 
 	@Override
-	public boolean remove(K k) {
-		if (root.getKey().equals(k))
-		{
-			Node<K,V> aux = root.getSuccesor();
-			aux.setLeft(root.getLeft());
-			root.getLeft().setParent(aux);
-			aux.setRight(aux.getRight());
-			root.getRight().setParent(aux);
-			
-			if (aux.getParent().getLeft() == aux) aux.getParent().setLeft(null);
-			else aux.getParent().setRight(null);
-			
-			root = aux;
-			size--;
+	public boolean update(K key, V value) {
+
+		Node<K, V> nodeToUpdate = searchNode(key);
+
+		if (nodeToUpdate != null) {
+			nodeToUpdate.setValue(value);
+
 			return true;
-		}else
-		{
-			return root.remove(k);
+
 		}
-		
+
+		else {
+
+			return false;
+
+		}
+
 	}
 
 	@Override
+	public boolean remove(K key) {
+		if (root != null) {
+
+			boolean r = removeRecursive(key, root, null);
+
+			if (r) {
+				setKeys.remove(key);
+			}
+
+			return r;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean removeRecursive(K key, Node<K, V> currentNode, Node<K, V> parent) {
+		if (currentNode != null) {
+			if (key.compareTo(currentNode.getKey()) < 0) {
+				return removeRecursive(key, currentNode.getLeft(), currentNode);
+			} else if (key.compareTo(currentNode.getKey()) > 0) {
+				return removeRecursive(key, currentNode.getRight(), currentNode);
+			} else {
+				if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+
+					if (currentNode == root) {
+						root = null;
+					} else {
+						if (parent.getRight() != null && parent.getRight() == currentNode) {
+							parent.setRight(null);
+						} else {
+							parent.setLeft(null);
+						}
+					}
+
+				} else if (currentNode.getRight() == null) {
+
+					if (currentNode == root) {
+						root.getLeft().setParent(null);
+						root = root.getLeft();
+					} else {
+						currentNode.getLeft().setParent(parent);
+
+						if (parent.getRight() != null && parent.getRight() == currentNode)
+							parent.setRight(currentNode.getLeft());
+
+						else
+							parent.setLeft(currentNode.getLeft());
+					}
+
+				} else if (currentNode.getLeft() == null) {
+
+					if (currentNode == root) {
+						root.getRight().setParent(null);
+						root = root.getRight();
+					} else {
+						currentNode.getRight().setParent(parent);
+
+						if (parent.getRight() != null && parent.getRight() == currentNode)
+							parent.setRight(currentNode.getRight());
+						else
+							parent.setLeft(currentNode.getRight());
+					}
+
+				} else {
+					Node<K, V> rightMin = getMin(currentNode.getRight());
+
+					currentNode.setKey(rightMin.getKey());
+					currentNode.setValue(rightMin.getValue());
+
+					removeRecursive(rightMin.getKey(), rightMin, rightMin.getParent());
+
+				}
+				weight--;
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	private Node<K, V> getMin(Node<K, V> node) {
+
+		while (node.getLeft() != null) {
+
+			node = node.getLeft();
+
+		}
+
+		return node;
+
+	}
+
+	@Override
+	public V search(K key) {
+
+		if (root != null) {
+
+			return searchRecursive(key, root);
+
+		}
+
+		else {
+
+			return null;
+
+		}
+
+	}
+
+	private V searchRecursive(K key, Node<K, V> currentNode) {
+
+		if (key.compareTo(currentNode.getKey()) == 0) {
+
+			return currentNode.getValue();
+
+		}
+
+		else if (key.compareTo(currentNode.getKey()) > 0) {
+
+			return searchRecursive(key, currentNode.getRight());
+
+		}
+
+		else {
+
+			return searchRecursive(key, currentNode.getLeft());
+
+		}
+
+	}
+
+	private Node<K, V> searchNode(K key) {
+		if (root != null) {
+
+			return searchNodeRecursive(key, root);
+
+		}
+
+		else {
+
+			return null;
+
+		}
+	}
+
+	private Node<K, V> searchNodeRecursive(K key, Node<K, V> currentNode) {
+
+		if (key.compareTo(currentNode.getKey()) == 0) {
+
+			return currentNode;
+
+		}
+
+		else if (key.compareTo(currentNode.getKey()) > 0) {
+
+			return searchNodeRecursive(key, currentNode.getRight());
+
+		}
+
+		else {
+
+			return searchNodeRecursive(key, currentNode.getLeft());
+
+		}
+
+	}
+
 	public boolean isEmpty() {
-		if (size == 0) return true;
-		return false;
+		return root == null;
 	}
 
 	@Override
-	public int size() {
-		
-		return size;
+	public boolean keyExists(K key) {
+
+		return setKeys.contains(key);
+
 	}
 
-	@Override
-	public boolean keyExists(K k) {
-		if (search(k) == null) return false;
-		return true;
-	}
-
-	@Override
-	public Node<K, V> searchNode(K k) {
-		if (root == null) return null;
-		else return root.search(k);	
-	}
-
-	@Override
-	public ArrayList<V> search(K k) {
-		Node<K,V> aux = searchNode(k);
-		
-		if (aux != null)
-		{
-			return aux.getValues();
-		}
-		return null;
-	}
-	
 }
